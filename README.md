249371924c4e339338da68889779c55d8a3f91cd
dkT430 <jiangminghao@iie.ac.cn>
2020-07-05 13:41:12 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # flowcontainer
-从pcap获取流的基本信息,例如包长序列、包到达时间序列、源IP/端口、目的IP/端口，开始和结束时间。
-
+Get basic flow information from pcap, such as packet length sequence, packet arrival time sequence, source IP/port, destination IP/port, start and end time.
---
+++ setup.py
@@ -0,0 +1,22 @@
+#coding:utf8
+__author__ = 'dk'
+import setuptools
+long_desp = ""
+with open("README.md","r") as fp:
+    long_desp = fp.read()
+setuptools.setup(
+    name="flowcontainer",
+    version="1.1",
+    author="Jiang Minghao",
+    author_email="jiangminghao@iie.ac.cn",
+    description="A python lib to parse traffic flow information from pcaps",
+    url="https://github.com/jmhIcoding/flowcontainer",
+    long_description=long_desp,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
