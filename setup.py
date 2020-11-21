ea290fb685a4d046069415feee9136b2718e09e1
dkT430 <jiangminghao@iie.ac.cn>
2020-11-21 10:00:41 +0800

    update

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ long_desp = \
 A python lib to parse traffic flow information from pcaps.\n
 Homepage : https://github.com/jmhIcoding/flowcontainer.\n
 Fix bugs:\n
-\t set the default filter string to be `tcp or udp or gre`
+\t set the default filter string to be `tcp or udp or gre`.\n
+\t update help information for errors. \n
 '''
 
 setuptools.setup(
     name="flowcontainer",
-    version="3.5",
+    version="3.6",
     author="Minghao Jiang",
     author_email="jiangminghao@iie.ac.cn",
     description="A python lib to parse traffic flow information from pcaps",
